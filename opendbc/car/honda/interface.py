91444e926fd3e054b818e236f8c13ac5aedd69f8
THERoen <199319559+THERoen@users.noreply.github.com>
2025-12-23 00:20:46 -0500

    Modify Clarity kp and ki to match other high torque cars
    Since torqueV is unchanged between 2X and 3X, looser tuning may not be needed.
    
    Comma's 2.5X Civic also uses 0.3, 0.1

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -297,7 +297,7 @@ class CarInterface(CarInterfaceBase):
           if fw.ecu == "eps" and b"-" not in fw.fwVersion and b"," in fw.fwVersion:
             stock_cp.lateralTuning.pid.kf = 0.00004
             stock_cp.lateralParams.torqueBP, stock_cp.lateralParams.torqueV = [[0, 5760, 15360], [0, 2560, 3840]]
-            stock_cp.lateralTuning.pid.kpV, stock_cp.lateralTuning.pid.kiV = [[0.1575], [0.05175]]
+            stock_cp.lateralTuning.pid.kpV, stock_cp.lateralTuning.pid.kiV = [[0.3], [0.1]]
           elif fw.ecu == "eps" and b"-" in fw.fwVersion and b"," in fw.fwVersion:
             stock_cp.lateralParams.torqueBP, stock_cp.lateralParams.torqueV = [[0, 5760, 10240], [0, 2560, 3840]]
             stock_cp.lateralTuning.pid.kpV, stock_cp.lateralTuning.pid.kiV = [[0.3], [0.1]]
